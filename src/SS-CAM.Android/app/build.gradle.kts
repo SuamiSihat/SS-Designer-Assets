2370fc2b0c5b347b59dba088e798c8d65953cd91
SuamiSihat <tech@suamisihat.com.my>
2026-09-08 12:16:24 +0800

    chore(android): bump versionCode to 467 for v4.6.2 release

--- src/SS-CAM.Android/app/build.gradle.kts
+++ src/SS-CAM.Android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.suamisihat.creative"
         minSdk = 26
         targetSdk = 35
-        versionCode = 466
+        versionCode = 467
         versionName = "4.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -75,6 +75,7 @@ dependencies {
     implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
+
 
 
 
